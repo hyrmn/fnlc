311264be59a63ccd37d5fef49d5515911a25d4de
Ben Hyrman <ben@hyr.mn>
2021-09-10 20:21:59 -0400

    Initial code

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Usage:\n\tlc \"path\\to\\file.txt\"");
+    return;
+}
+
+if(!File.Exists(args[0]))
+{
+    Console.WriteLine($"Could not find {args[0]}. Check the file path.");
+    return;
+}
+
+const int BufferSize = 512 * 1024;
+const byte Rune = (byte)'\n';
+
+using var file = new FileStream(args[0], FileMode.Open, FileAccess.Read, FileShare.None, bufferSize: BufferSize);
+
+var count = CountLines(file);
+Console.WriteLine(count);
+
+static unsafe uint CountLines(FileStream file)
+{
+    uint count = 0;
+
+    const int vectorSize = 256 / 8; //256 bits, 8 bits in a byte.
+    var maskSrc = stackalloc byte[vectorSize];
+    var scratch = stackalloc byte[vectorSize];
+
+    for (var i = 0; i < vectorSize; i++)
+    {
+        maskSrc[i] = Rune;
+    }
+    
+    var runeMask = Avx2.LoadVector256(maskSrc);
+    var zero = Vector256<byte>.Zero;
+    var accumulator = Vector256<long>.Zero;
+
+    long bytesRead = 0;
+    var fileSize = file.Length;
+
+    int read;
+
+    var buffer = new byte[BufferSize];
+
+    while (bytesRead < fileSize)
+    {
+        read = file.Read(buffer, 0, BufferSize);
+        bytesRead += read;
+        int i;
+        fixed (byte* ptr = buffer)
+        {
+            for (i = 0; i <= read - vectorSize; i += vectorSize)
+            {
+                var v = Avx2.LoadVector256(ptr + i);
+                var masked = Avx2.CompareEqual(v, runeMask);
+                var result = Avx2.MoveMask(masked);
+                count += Popcnt.PopCount((uint)result);
+            }
+        }
+    }
+
+    return count;
+}
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# fnlc
-A line-counter written in C# and using Intrinsics
+# Line Counter
+
+This is a small command-line utility to count lines in a file. That's it. That's all it does. Technically, it doesn't even care if it's text. 
+
+This is the .NET 6.0 + AVX2 version of my [other .NET line counter](https://github.com/hyrmn/nlc).
+
+There are some counting assumptions that I made. I had originally chosen to have this match my editor's line count. That is, if Visual Studio Code shows `x` lines then my logic would also show `x` lines. However, I've chosen to follow the behavior of `wc -l`. I count carriage returns (`\n`). If a file does not end with a carriage return then the last line will not be counted.
+
+## How to Build
+
+Clone this repository and then run `dotnet build` from the solution root.
+
+```posh
+> dotnet build -c Release --nologo
+```
+
+This will create a release build of the utility at `/bin/Release/net6.0/nlc.exe`. From there, you will need to copy it to a place in your `%PATH%`.
+
+## How to Use
+
+Unlike `nlc`, `fnlc` can only parse files.
+
+To read a file:
+
+```
+> fnlc "path/to/your/file.txt"
+```
+
+The only output from `fnlc` will be the line count, or the error (errors are not written to STDERR)
+
+So the full run might look like 
+
+```
+> fnlc "path/to/your/file.txt"
+109
+```
